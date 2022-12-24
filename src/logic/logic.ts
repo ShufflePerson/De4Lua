13e8341da4246bb9d2fd379b88ee4f8f217fdf38
Shuffle21 <>
2022-12-24 12:47:06 +0300

    Fixed a error in rare cases

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -17,7 +17,7 @@ import rename_local_globals from './static/rename_local_globals';
 function logic(chunk: luaparse.Chunk, cycles: number = 0): luaparse.Chunk {
     console.log(`[+] Logic cycle ${cycles}`)
 
-    let newchunk: luaparse.Chunk;
+    let newchunk: luaparse.Chunk = chunk;
     
     //Calls all of the deobfucation methods.
     //As stated in the README, TODO: Configs, so you can toggle on and off differnt deobfuscation methods
